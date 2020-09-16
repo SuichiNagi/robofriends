fbd013b07cbf9bda0f902b54eb2f482dfe86c17f
Kim Nuqui <68434908+SuichiNagi@users.noreply.github.com>
2020-09-17 02:08:54 +0800

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Jose Victory',
+	username: 'Josev',
+	email: 'victory@ichiwa.biz'
+},
+{
+	id: 2,
+	name: 'Koga Loser',
+	username: 'Kolos',
+	email: 'kolos@ic.tv'
+},
+{
+	id: 3,
+	name: 'Koko Krunch',
+	username: 'Kokru',
+	email: 'koko@crunchy.net'
+},
+{
+	id: 4,
+	name: 'Abanoy Manonoy',
+	username: 'Abnos',
+	email: 'abno@nono.org'
+},
+{
+	id: 5,
+	name: 'It Really Hurts',
+	username: 'Hurtiexz',
+	email: 'ithurts@toktik.ca'
+},
+{
+	id: 6,
+	name: 'Kawabongga Turtle',
+	username: 'Njaturtle',
+	email: 'ninjas@tut.ba'
+},
+{
+	id: 7,
+	name: 'Jabawock Ez',
+	username: 'Jaba',
+	email: 'jabawock@dance.cw'
+},
+{
+	id: 8,
+	name: 'King Mania',
+	username: 'Kingu',
+	email: 'mania@kingu.kg'
+},
+{
+	id: 9,
+	name: 'Blaster Boom',
+	username: 'Blast',
+	email: 'boombooms@boom.bm'
+},
+{
+	id: 10,
+	name: 'Kapow Ching',
+	username: 'Chingaling',
+	email: 'chingkongs@chong.ch'
+},
+]
